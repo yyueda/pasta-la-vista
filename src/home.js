d238e4dee05af19e6b35d2feb05efee00e1ae7c1
yyueda <yyuedaa@gmail.com>
2024-10-24 16:08:15 +0800

    Basic styling for homepage

---
+++ src/home.js
@@ -0,0 +1,5 @@
+function createNav() {
+    homeButton = document.querySelector('#home-button');
+    menuButton = document.querySelector('#menu-button');
+    aboutButton = document.querySelector('#about-button');
+}
