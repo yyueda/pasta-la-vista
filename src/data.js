33fb751f3952f81966a433b7294184e77eb7a249
yyueda <yyuedaa@gmail.com>
2024-10-25 10:17:58 +0800

    Add pasta dishes to data

---
+++ src/data.js
@@ -0,0 +1,50 @@
+import aglioOlio from "./src/images/aglio-olio.png";
+import carbonara from "./src/images/carbonara.png";
+import linguineVongole from "./src/images/linguine-vongole.png";
+import beefRagu from "./src/images/beef-ragu.png";
+import penneArrabbiata from "./src/images/penne-arrabbiata.png";
+import mushroomRisotto from "./src/images/truffle-mushroom-risotto.png";
+
+class Dish {
+
+    constructor(name, description, imageSrc) {
+        this.name = name;
+        this.description = description;
+        this.imageSrc = imageSrc;
+    }
+}
+
+const pastas = [
+    new Dish(
+        "Spaghetti Aglio Olio",
+        "Simple and savory pasta with garlic, chilli and extra virgin olive oil",
+        aglioOlio
+    ),
+    new Dish(
+        "Spaghetti Carbonara",
+        "Classic pasta with crispy italian smoked pancetta, egg, and parmesan cheese.",
+        carbonara
+    ),
+    new Dish(
+        "Linguini Vongole",
+        "Linguini with fresh clams, prawns, cherry tomato, garlic, white wine and a touch of chilli",
+        linguineVongole
+    ),
+    new Dish(
+        "Beef Ragu",
+        "Tender beef in a rich tomato and red wine sauce, served over fresh pasta.",
+        beefRagu
+    ),
+    new Dish(
+        "Penne Arrabbiata",
+        "Penne tossed in a spicy tomato sauce with garlic and chili flakes.",
+        penneArrabbiata
+    ),
+    new Dish(
+        "Truffle Mushroom Risotto",
+        "Creamy risotto with earthy mushrooms and a hint of truffle essence.",
+        mushroomRisotto
+    ),
+];
+
+export default pastas;
